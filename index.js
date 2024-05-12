b9e80fdefc97154dcefb0315c60dd2136629f25c
Martín Molina <martindmolina@live.com.ar>
2024-05-12 04:53:29 +0000

    async middleware and code optimization

--- index.js
+++ index.js
@@ -1,82 +1,71 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
-
-// My code
-const bodyParser = require('body-parser');
 const url = require('url');
 const dns = require('dns');
 const mongoose = require('mongoose');
 
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 mongoose.connect(process.env.MONGO_URI);
 
 var shortenedURLSchema = new mongoose.Schema({
-  original_url: {type: String},
-  short_url: {type: Number}
+  original_url: String,
+  short_url: Number
 });
+
 var shortenedURL = mongoose.model('shortenedURL', shortenedURLSchema);
 
-app.post('/api/shorturl', bodyParser.urlencoded({extended: true}), (req, res) => {
+// front page
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// request short URL
+app.post('/api/shorturl', (req, res) => {
   try {
     var link = new URL(req.body.url);
   }
-  catch { // If URL is invalid
+  catch { // if URL is invalid
     res.json({error: "Invalid URL"});
     return;
   }
-  dns.lookup(link.hostname, (err, address) => {
-    
-    if (!address) // If host is invalid
-      res.json({error: "Invalid URL"});
-    else {
-
-      // Fetch shortenedURL of the posted URL
-      shortenedURL.findOne({original_url: link.href})
-      .then((url) => {
-
-        if(!url) { // If it doesn't exist
-
-          // Create and send new shortenedURL document
-          shortenedURL.countDocuments()
-          .then((quantity) => {
-            new shortenedURL({original_url: link.href, short_url: quantity}).save();
-            res.json({original_url: link.href, short_url: quantity});
-          });
-        }
-        else // If it exists
-          res.json({original_url: link.href, short_url: url.short_url});
-      });
+  dns.lookup(link.hostname, async (err, address) =>
+  {
+    if (!address) // if host is invalid
+      res.json({error: "Invalid Hostname"});
+    else
+    {
+      const url = await shortenedURL.findOne({original_url: link.href});
+      if(url) // fetch existing short URL
+        res.json({original_url: link.href, short_url: url.short_url});
+      else
+      { // create new short URL
+        const count = await shortenedURL.countDocuments();
+        new shortenedURL({original_url: link.href, short_url: count}).save();
+        res.json({original_url: link.href, short_url: count});
+      }
     }
   });       
 });
 
-app.get('/api/shorturl/:value', (req, res) => {
-  shortenedURL.findOne({short_url: req.params.value})
-  .then((sUrl) => {
-    if(sUrl)
-      res.redirect(sUrl.original_url);
-    else
-      res.json({error: "Short URL does not exist"});
-  });
+// redirect to shortened URL
+app.get('/api/shorturl/:value', async (req, res) => {
+  const url = await shortenedURL.findOne({short_url: req.params.value});
+  if(url)
+    res.redirect(url.original_url);
+  else
+    res.json({error: "Short URL does not exist"});
 });
